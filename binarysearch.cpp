ed10e8bfa28ce48f343fa7a9535cd22d0f214816
PrasanBora <prasanbora23@gmail.com>
2022-08-09 22:00:07 +0530

    done👽💀👽

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-
+//using while loop
 int binarysearch(int arr[],int size, int key)
 {
     int start=0;
@@ -30,6 +30,18 @@ int binarysearch(int arr[],int size, int key)
     return -1;
 }
 
+//using recursion but taking start and end from user --
+int binarysearch_rec(int arr[],int start,int end, int key)
+{
+    int mid=start+(end-start)/2;
+    if(arr[mid]==key)
+     return mid;
+     if(arr[mid]<key)
+        binarysearch_rec(arr,mid+1,end,key);
+     else 
+        binarysearch_rec(arr,start,mid-1,key);   
+
+}
 int main()
 {
     int list[]={1,3,5,7,8,9};
