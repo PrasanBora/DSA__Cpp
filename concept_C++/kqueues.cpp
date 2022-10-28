4c5a1db71957458f61f667551c3b15fe4def37d3
PrasanBora <prasanbora23@gmail.com>
2022-10-28 14:24:01 +0530

    📍comment added

--- concept_C++/kqueues.cpp
+++ concept_C++/kqueues.cpp
@@ -21,11 +21,13 @@ public:
     arr = new int [size];
     next=new int [size];
     freespot=0;
-
+    // default value of next will be the index of arr
     for(int i=0;i<size-1;i++)
         next[i]=i+1;
-
+    // for last element no element exist therfore putting -1 
     next[size-1]=-1;
+
+    // default values of front and rear for queues 
     for(int j=0;j<nq;j++)
     {
         front[j]=-1;
@@ -35,39 +37,50 @@ public:
 
   void enqueue(int val ,int qn)
   {
+    // is space is available thatis any freespot is present or not 
     if(freespot==-1)
     {
         cout<<"Overflow ! Arr is full"<<endl;
         return;
     }
-     int index= freespot;
+    // store freespot
+      int index= freespot;
+    // update the freespot that is next for index
       freespot=next[index];
 
+    // check is pushing first element to queue
      if(front[qn-1]==-1)
-        front[qn-1]=index;
+        front[qn-1]=index; // updating the front as its first element 
+
      
      else 
-        next[rear[qn-1]]=index;
+        next[rear[qn-1]]=index; // making the link between last element 
+                                 //and new element using the next arr
       
-        arr[index]=val;
-        rear[qn-1]=index;
-        next[index]=-1;
-       
-
+        arr[index]=val; // store val to arr
+        rear[qn-1]=index; // updating the rear for queue
+        next[index]=-1;  // marking the next of current index as -1 representing a 
+                         // a value is there 
+    
         cout<< " ELEMENT  ADDED "<<endl;
   }
   
   void dequeue(int qn)
   {
+    // check if queue is empty or not 
      if(front[qn-1]==-1)
     {
         cout<<"Underflow ! queue is empty"<<endl;
         return;
     }
+    // store front of queue to be deleted
     int index = front[qn-1];
+    // update front of queue
     front[qn-1]=next[index];
-
+     
+    // create link btw deleted space and freespot 
     next[index]=freespot;
+    // update freespot to index 
     freespot=index;
 
     cout<<"deleted - "<<arr[index]<<" from q - "<<qn <<endl;
