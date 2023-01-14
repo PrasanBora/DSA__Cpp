a6c58da86a00405eab2bd1966fb59c7cc91814df
PrasanBora <prasanbora23@gmail.com>
2023-01-14 22:35:24 +0530

    heap

---
+++ concept_C++/heap/heap_implementation.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+  class heap 
+  {
+    public:
+    int arr[100];
+    int size =0;
+
+    void insert (int val )
+    {
+      size =size+1;
+      int index = size;
+      arr[index]=val ;
+       while (index> 1)
+       {
+        int parent = index/2;
+        if(arr[parent]<arr[index])
+        {
+          swap(arr[parent],arr[index]);
+          index=parent; 
+        }
+         else 
+         return;
+       }
+    }
+  };
+int main()
+{
+
+    return 0;
+}
