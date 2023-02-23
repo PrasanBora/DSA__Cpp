6d04e73391ffdf15641fbbd2933d095dbfbb4521
PrasanBora <prasanbora23@gmail.com>
2023-02-23 13:31:57 +0530

    heap

--- concept_C++/heap/heap_implementation.cpp
+++ concept_C++/heap/heap_implementation.cpp
@@ -7,7 +7,15 @@ using namespace std;
     public:
     int arr[100];
     int size =0;
-
+    
+    void print()
+    {  
+      cout<<endl;
+      for(int i=1;i<=size;i++)
+      {
+        cout<<arr[i]<<" ";
+      }
+    }
     void insert (int val )
     {
       size =size+1;
@@ -25,9 +33,102 @@ using namespace std;
          return;
        }
     }
+     void delet()
+     {
+      cout<<"delete_called";
+       arr[1]=arr[size];
+       size--;
+       int i=1;
+       while(size>i)
+       {
+         int left = 2*i ;
+         int right =2*i +1;
+
+         if(left <size && arr[i]<arr[left])
+            {
+              swap( arr[i],arr[left]);
+              i=left;
+            }
+         else if(right <size && arr[i]<arr[right])
+            {
+              swap( arr[i],arr[right]);
+              i=right;
+            }
+            else 
+            return ;
+       }
+     }
+
+     // heapify function 
+
+     void heapify(int arr[],int n, int i)
+     {
+       int largest =i;
+       int left = 2*i+1;
+       int right =2*i+2; 
+       
+       if(left<n && arr[largest]<arr[left])
+       {
+         largest=left;
+       }
+       if(right<n && arr[largest]<arr[right])
+       {
+         largest=right;
+       }
+
+       if(largest!=i)
+       {
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
+       }
+
+     }
+
+     void heapsort(int arr[],int n)
+     {
+      //making a max heap 
+       for(int i=n/2 -1;i>=0;i--)
+       {
+        heapify(arr,n,i);
+       }
+
+       for(int i=n-1;i>=0;i--)
+       {
+        swap(arr[0],arr[i]);
+         heapify(arr,i,0);
+       }
+     }
+
   };
 int main()
 {
+    heap h;
+    h.insert(10);
+    h.insert(12);
+    h.insert(0);
+    h.insert(1);
+    h.insert(19);
+    h.insert(13);
 
+    h.print();
+    h.delet();
+
+    h.print();
+    
+    int arr2[] = { -1,12, 11, 13, 5, 6, 7 };
+    int N = sizeof(arr2) / sizeof(arr2[0]);
+      cout<<endl<<"-----------"<<endl;
+    for(int i=0 ;i<N;i++)
+    {
+      cout<<arr2[i]<<" ";
+    }
+   
+    cout<<endl<<"heap sort called "<<endl;
+  
+    h.heapsort(arr2,N);
+    for(int i=0 ;i<N;i++)
+    {
+      cout<<arr2[i]<<" ";
+    }
     return 0;
 }
---
+++ concept_C++/trie/trie.cpp
@@ -0,0 +1,98 @@
+//trie implementation 
+#include<bits/stdc++.h>
+using namespace std;
+
+class trienode
+{
+    public :
+    char data ;
+    trienode* children[26];
+    bool isterminal ;
+
+    trienode(char ch )
+    {
+        data =ch;
+        for(int i=0;i<26;i++)
+        {
+            children[i]=NULL;
+        }
+        isterminal = false;
+    }
+};
+class trie 
+{
+  public:
+  trienode * root;
+   trie()
+   {
+    root = new trienode('\0');
+   }
+  void inserttrie(trienode * root ,string str)
+  {
+    //base case 
+    // cout<<str[0]<<endl;
+    if(str.length()==0)
+    {
+        root->isterminal =true;
+        return;
+    }
+    //getting the char 
+    int index = str[0]-'A';
+    trienode * child ;
+    // if value is present just move pointer to next char 
+    if(root->children[index] !=NULL)
+    {
+        child=root->children[index];
+    }
+    else // absent 
+    {
+        cout<<"newnode"<<"--"<<str[0]<<endl;
+        child=new trienode(str[0]);
+        root->children[index]=child;
+    }
+    inserttrie(child,str.substr(1));
+
+  }
+  // function to call insertrie
+  void insert(string st)
+  {
+    inserttrie(root,st);
+  }
+  // function for search 
+  bool search(string str)
+  {
+     return searchtrie(root,str);
+  }
+  
+  bool searchtrie(trienode * root ,string str )
+  {
+   if(str.length()==0)
+    {
+        return root->isterminal ;
+    }
+    //getting the char 
+    int index = str[0]-'A';
+    trienode * child ;
+    // if value is present just move pointer to next char 
+    if(root->children[index])
+    {
+        child=root->children[index];
+    }
+    else // absent 
+    {
+       return false;
+    }
+    //  cout<<"search "<<index;
+     return searchtrie(child,str.substr(1));
+  }
+
+};
+int main()
+{
+    trie *t =new trie();
+    t->insert("HELOL");
+    t->insert("HELI");
+    t->insert("HEJ");
+    cout<<" present : "<<t->search("HELOLJJ");
+   
+}
